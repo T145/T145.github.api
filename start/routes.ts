568016c6264fc1e9c0e466443e131da99b246aa7
T145 <T145@protonmail.com>
2020-09-15 00:18:26 -0400

    Configured MetalChests stats endpoint

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,44 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route';
+import axios from 'axios';
+
+function numberWithCommas(x: number) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 Route.get('/', async () => {
   return { hello: 'world' };
+});
+
+Route.get('/metalchests', async () => {
+  try {
+    interface GameVersion {
+      gameVersion: string;
+      projectFileId: number;
+      projectFileName: string;
+      fileType: number;
+      gameVersionFlavor: undefined;
+    }
+
+    const resp = await axios({
+      method: 'get',
+      url: 'https://addons-ecs.forgesvc.net/api/v2/addon/290145',
+      responseType: 'json',
+    });
+    const data = resp.data;
+    const gameVersions: Array<GameVersion> = data.gameVersionLatestFiles;
+    const versions: string[] = [];
+
+    gameVersions.forEach(version => {
+      versions.push(version.gameVersion);
+    });
+
+    return {
+      downloadCount: numberWithCommas(data.downloadCount),
+      versions: versions,
+    };
+  } catch (err) {
+    return { error: err };
+  }
 });
